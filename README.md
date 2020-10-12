5d2add099eeb15c3fcb44d514f85308228915aea
KisonHe <kisonmaxine@gmail.com>
2020-10-12 22:42:33 +0800

    V0.4. DarkMode

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ An App to work with DormLock Project to open door
 1. Finish Admin fragment related functions like
    1. Change Password of any user
    2. Set servo Up and Down Limits
-2. Use [AndroidX Preference Library](https://developer.android.com/guide/topics/ui/settings)
-   1. Remove the admin fragment, use `Control Preference visibility`
+2. Apply better design and resource for dark mode
 
 
 ## Known Bug
-1. In use of volley and NavController, if navigated to another fragment then the volley handler from last fragment
-calls NavController, things will crash
+1. In use of volley and NavController, if navigated to another fragment then the volley handler from last fragment calls NavController, things will crash
--- app/src/main/java/xyz/kisonhe/dormlockapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/xyz/kisonhe/dormlockapp/ui/settings/SettingsFragment.java
@@ -4,14 +4,16 @@ package xyz.kisonhe.dormlockapp.ui.settings;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import xyz.kisonhe.dormlockapp.ChangePasswordActivity;
-import xyz.kisonhe.dormlockapp.MainActivity;
 import xyz.kisonhe.dormlockapp.R;
 
-
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO;
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -28,6 +30,26 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 Intent intent = new Intent(requireActivity(), ChangePasswordActivity.class);
                 startActivity(intent);
 
+                return false;
+            }
+        });
+
+        Preference DarkModePreference = findPreference("DarkModeConfig");
+        DarkModePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if (newValue.equals("MODE_NIGHT_FOLLOW_SYSTEM")) {
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM);
+                    return true;
+                }
+                if (newValue.equals("MODE_NIGHT_NO")) {
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO);
+                    return true;
+                }
+                if (newValue.equals("MODE_NIGHT_YES")) {
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
+                    return true;
+                }
                 return false;
             }
         });
