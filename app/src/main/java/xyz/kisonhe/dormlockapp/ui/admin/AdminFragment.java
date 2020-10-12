c92f9a23abc91d05525d2ecb8310b4df1aa9a52e
KisonHe <kisonmaxine@gmail.com>
2020-10-12 20:23:18 +0800

    LastBackUpBeforeChanges

--- app/src/main/java/xyz/kisonhe/dormlockapp/ui/admin/AdminFragment.java
+++ app/src/main/java/xyz/kisonhe/dormlockapp/ui/admin/AdminFragment.java
@@ -16,28 +16,6 @@ import androidx.navigation.Navigation;
 import xyz.kisonhe.dormlockapp.R;
 
 
-//
-//public class FireMissilesDialogFragment extends DialogFragment {
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        // Use the Builder class for convenient dialog construction
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        builder.setMessage("R.string.dialog_fire_missiles")
-//                .setPositiveButton("R.string.fire", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // FIRE ZE MISSILES!
-//                    }
-//                })
-//                .setNegativeButton("R.string.cancel", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // User cancelled the dialog
-//                    }
-//                });
-//        // Create the AlertDialog object and return it
-//        return builder.create();
-//    }
-//}
-
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AdminFragment#newInstance} factory method to
